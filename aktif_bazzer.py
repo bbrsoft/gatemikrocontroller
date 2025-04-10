38a94acbcb40af1b4843280da21f5b7e0a9657a3
bbrsoft <164309399+bbrsoft@users.noreply.github.com>
2025-04-10 07:23:51 +0700

    Update aktif_bazzer.py

--- aktif_bazzer.py
+++ aktif_bazzer.py
@@ -1,14 +1,10 @@
 import RPi.GPIO as GPIO
-import time
-
-BUZZER_PIN = 17  # Gunakan GPIO17
-
+ 
+buzzer = 18
+ 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUZZER_PIN, GPIO.OUT)
-
-# Nyalakan buzzer selama 1 detik
-GPIO.output(BUZZER_PIN, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(BUZZER_PIN, GPIO.LOW)
-
-GPIO.cleanup()
+ 
+GPIO.setup(buzzer, GPIO.OUT)
+ 
+GPIO.output(buzzer, GPIO.HIGH)
